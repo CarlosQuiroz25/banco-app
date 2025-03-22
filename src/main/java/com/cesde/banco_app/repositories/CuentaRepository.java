19f17c580fa559017e851841fc9eb944271100fa
CarlosQuiroz25 <caquirozm@cesde.net>
2025-03-22 11:26:05 -0500

    Se agregan los repositorios para cada entidad

---
+++ src/main/java/com/cesde/banco_app/repositories/CuentaRepository.java
@@ -0,0 +1,9 @@
+package com.cesde.banco_app.repositories;
+
+import com.cesde.banco_app.models.Cuenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CuentaRepository extends JpaRepository<Cuenta, Integer> {
+}
---
+++ src/main/java/com/cesde/banco_app/repositories/TransaccionRepository.java
@@ -0,0 +1,9 @@
+package com.cesde.banco_app.repositories;
+
+import com.cesde.banco_app.models.Transaccion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransaccionRepository extends JpaRepository<Transaccion, Integer> {
+}
